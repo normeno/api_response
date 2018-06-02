53a2a66d238b9fc7b99cb7ea7f948504cdd670be
Nicolas Ormeno <ni.ormeno@gmail.com>
2018-06-01 20:34:33 -0400

    Add comments

--- src/ApiResponseServiceProvider.php
+++ src/ApiResponseServiceProvider.php
@@ -1,9 +1,36 @@
 <?php
-
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+/**
+ * This file is part of the Api Response library.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * PHP Version 7
+ *
+ * LICENSE: This source file is subject to the MIT license that is available
+ * through the world-wide-web at the following URI:
+ * http://opensource.org/licenses/mit-license.php
+ *
+ * @category Src
+ * @package  Normeno\ApiResponse
+ * @author   Nicolas Ormeno <ni.ormeno@gmail.com>
+ * @license  http://opensource.org/licenses/mit-license.php MIT License
+ * @link     https://github.com/normeno/api_response
+ */
 namespace Normeno\ApiResponse;
 
 use Illuminate\Support\ServiceProvider;
 
+/**
+ * Tests for Format
+ *
+ * @category Test
+ * @package  Normeno\ApiResponse\Test
+ * @author   Nicolas Ormeno <ni.ormeno@gmail.com>
+ * @license  http://opensource.org/licenses/mit-license.php MIT License
+ * @link     https://github.com/normeno/api_response
+ */
 class ApiResponseServiceProvider extends ServiceProvider
 {
     /**
